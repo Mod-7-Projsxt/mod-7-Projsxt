0f3a165c9bcd8a6fc513994d653489954126a7ea
Fiona Baci <fionabaci04@gmail.com>
2024-08-23 13:50:44 -0400

    rendered flags and google link

--- vite-project/src/CountryFetchCard.jsx
+++ vite-project/src/CountryFetchCard.jsx
@@ -2,23 +2,49 @@ import React, { useContext, useEffect, useState } from 'react';
 import handleFetch from './utils/fetch';
 import CountryProvider from './context/countriesProvider';
 import CountriesContext from './context/context';
+import FlagCard from './FlagFetch';
+import {Link} from 'react-router-dom'
 
 function CountryCard() {
 
   const countriesData = useContext(CountriesContext).countriesData
+  const flags = useContext(CountriesContext).flagData
+
+
+
+  
   return (
-    <div className="countries">
+    <>
+    <div>
+    
+    </div>
+    <div title='click me' className="countries" href='{}'>
+    {/* {flags.map(flag => (<img src={flag.flags.svg} alt=""/>))  } */}
       {countriesData?.map(country => ( // if the countriesData is an array
-        <div key={country.cca3} className="country">
-          <h2>{country.name.common}</h2>
+        <div key={country.index} className="country">
+          <h2>{country.name.common} {country.flag}</h2>
+          <img src={country.flags.svg} alt="" />
           <p>Capital: {country.capital ? country.capital[0] : 'N/A'}</p>
-          <p>Region: {country.region}</p>
+          <p>Region: {country.region}<span></span></p>
           <p>Population: {country.population}</p>
-          {/* <p>Language: {country.languages}</p>
-          <p>Currency: {country.currencies}</p> */}
+          <p>Language(s): {country.languages ? Object.values(country.languages).join(', ') : 'N/A'} </p>
+          <p>
+              <a
+                href={country.maps.googleMaps}
+              >
+                View on Google Maps
+              </a>
+            </p>
+          <p>Currency: {country.currencies ? Object.values(country.currencies)
+          
+          .map(currency => `${currency.name} (${currency.symbol})`).join(', ') : 'N/A'}</p>
+    
         </div>
       ))}
+      
     </div>
+
+    </>
   );
 }
 
--- vite-project/src/GoogleMapFetch.jsx
+++ vite-project/src/GoogleMapFetch.jsx
@@ -1,20 +1,21 @@
+import React, { useContext, useEffect, useState } from 'react';
+import handleFetch from './utils/fetch';
+import fetchFlagImages from './context/countriesProvider';
+import CountriesContext from './context/context';
+
+
 function googleMapFetch() {
 
-    const countriesData = useContext(CountriesContext).countriesData
+    const flagData = useContext(CountriesContext).countriesData
     return (
       <div className="countries">
-        {countriesData?.map(country => ( // if the countriesData is an array
-          <div key={country.cca3} className="country">
-            <h2>{country.name.common}</h2>
-            <p>Capital: {country.capital ? country.capital[0] : 'N/A'}</p>
-            <p>Region: {country.region}</p>
-            <p>Population: {country.population}</p>
-            <p>Language: {country.languages}</p>
-            <p>Currency: {country.currencies}</p>
+        {countriesData?.map(country => (
+          <div className="maps">
+            <h2>{}</h2>
           </div>
         ))}
       </div>
     );
   }
   
-  export default CountryCard;
+  export default googleMapFetch;
